45295bac68fa9479b657c5b63c5c46edc0cc6e7a
Alexandr Korovaychenko <puls.ek@rambler.ru>
2021-08-28 16:21:21 +0300

    Omit

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -6,4 +6,20 @@
  */
 export const omit = (obj, ...fields) => {
 
+			let massElements = Object.entries(obj); 
+			let massPosNum = []; // для хранения последовательности найденных полей в Объекте
+			let ObjNew = {};
+						
+			for (let i = 0; i < massElements.length; i++) {
+					if(fields.indexOf(massElements[i][0]) == -1) { massPosNum.push(i); }
+				}
+				
+				massPosNum.sort(); // Сортируем чтобы последовательность совпадала с начальным объектом
+				
+				for (let PosNum of massPosNum){
+						ObjNew[massElements[PosNum][0]]	= massElements[PosNum][1];
+					}
+					
+				return ObjNew;
+				
 };
