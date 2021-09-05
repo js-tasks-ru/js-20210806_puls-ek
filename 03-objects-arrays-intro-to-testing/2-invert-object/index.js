939f9292ed7c25d5206e8cc7574ea4d54e869c7d
Alexandr Korovaychenko <puls.ek@rambler.ru>
2021-09-05 09:25:51 +0300

    invertObj

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,15 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-
+	
+	if(arguments.length > 0){
+	
+		const massTwo = (Object.entries(obj));
+						
+		massTwo.forEach((item, index, array) => {
+				item.sort(()=> -1); 
+			});				
+					
+		return Object.fromEntries(massTwo);
+	}
 }
